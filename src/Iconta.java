945ff5fd92869fe45a72c4927c618669c844f56f
marialourenco <maryhenrique9648@gmail.com>
2024-06-12 23:10:16 -0300

    Conclusao Banco

--- src/Iconta.java
+++ src/Iconta.java
@@ -1,3 +1,14 @@
 public interface Iconta {
 
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor,Iconta contaDestino);
+    
+    void imprimirInfoComuns();
+    
+    void imprimirExtrato();
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Cliente Maria = new Cliente();
+        Maria.setNome(("Maria"));
+
+        Conta cc = new ContaCorrente(Maria);
+        Conta cp = new ContaPoupanca(Maria);
+
+        cc.depositar(100);
+        cc.transferir(75, cp);
+
+        cc.imprimirExtrato();
+        cc.imprimirInfoComuns();
+
+        cp.imprimirExtrato();
+        cp.imprimirInfoComuns();
+
     }
 }
